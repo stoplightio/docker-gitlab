b4b75c942f1c8b58adda71cb09c726a746bb3c12
Ross McDonald <ross@stoplight.io>
2018-03-25 21:26:24 -0500

    Fix yarn install

--- assets/build/install-node.sh
+++ assets/build/install-node.sh
@@ -2,6 +2,3 @@
 
 # install node
 rpm -Uvh https://rpm.nodesource.com/pub_8.x/el/7/x86_64/nodejs-8.9.4-1nodesource.x86_64.rpm
-
-# install yarn
-curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version 1.3.2
--- assets/build/install.sh
+++ assets/build/install.sh
@@ -133,8 +133,9 @@ exec_as_git cp ${GITLAB_INSTALL_DIR}/config/gitlab.yml.example ${GITLAB_INSTALL_
 exec_as_git cp ${GITLAB_INSTALL_DIR}/config/database.yml.mysql ${GITLAB_INSTALL_DIR}/config/database.yml
 
 # Installs nodejs packages required to compile webpack
-exec_as_git yarn install --production --pure-lockfile
-exec_as_git yarn add ajv@^4.0.0
+exec_as_git curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version 1.3.2
+exec_as_git ~/.yarn/bin/yarn install --production --pure-lockfile
+exec_as_git ~/.yarn/bin/yarn add ajv@^4.0.0
 
 echo "Compiling assets. Please be patient, this could take a while..."
 exec_as_git bundle exec rake gitlab:assets:compile USE_DB=false SKIP_STORAGE_VALIDATION=true
