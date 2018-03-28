a59ffd925eceda1996d86bd9b41ec39b6b28798e
Ross McDonald <ross@stoplight.io>
2018-03-28 12:06:25 -0500

    Move back to gitlab dir when issuing bundle and rake commands.

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,11 +15,13 @@ case ${1} in
 
     case ${1} in
       app:start)
+        cd ${GITLAB_INSTALL_DIR}
         migrate_database
         rm -rf ${GITLAB_HOME}/tmp/supervisord/supervisor.sock
         exec /usr/bin/supervisord -c ${SUPERVISOR_CONF}
         ;;
       app:init)
+        cd ${GITLAB_INSTALL_DIR}
         migrate_database
         ;;
       app:sanitize)
@@ -27,6 +29,7 @@ case ${1} in
         ;;
       app:rake)
         shift 1
+        cd ${GITLAB_INSTALL_DIR}
         execute_raketask $@
         ;;
     esac
